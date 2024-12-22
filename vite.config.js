97b1995612e3c00ca2b2974c116ccb21eb85e5a4
Chloe <chloe.hsu.2024@gmail.com>
2024-12-22 18:38:02 +0800

    環境建置

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //開發中、產品路徑
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? 
+  '/React-gh-pages-sample/' : '/',
   plugins: [react()],
 })
